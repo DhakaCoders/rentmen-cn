7ef0443c40690d20a0ef9e65085d168361ba0028
Rannojit <cserannojitsarkar22@gmail.com>
2020-02-28 02:36:52 +0600

    inner page desktop view done

--- themes/rentmen/assets/js/main.js
+++ themes/rentmen/assets/js/main.js
@@ -154,7 +154,83 @@ google.maps.event.addDomListener(window, 'load', initialize);
 /*Rannojit*/
 
 
+if( $('.dft-question-mark-slider').length ){
+    $('.dft-question-mark-slider').slick({
+      dots: true,
+      infinite: false,
+      arrow: false,
+      speed: 300,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
 
+if( $('.dft-blog-slider').length ){
+    $('.dft-blog-slider').slick({
+      dots: true,
+      infinite: false,
+      arrow: false,
+      speed: 300,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
 
 
     new WOW().init();
