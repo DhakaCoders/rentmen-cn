16b4b5af93d329013a4a9505ee2c22ffcf1eff8f
Rannojit <cserannojitsarkar22@gmail.com>
2020-03-01 02:58:38 +0600

    responsive start

--- themes/rentmen/assets/js/main.js
+++ themes/rentmen/assets/js/main.js
@@ -340,19 +340,10 @@ if( $('.dft-question-mark-slider').length ){
       slidesToScroll: 1,
       responsive: [
         {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
+          breakpoint: 768,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToScroll: 1
           }
         },
         {
@@ -379,19 +370,10 @@ if( $('.dft-blog-slider').length ){
       slidesToScroll: 1,
       responsive: [
         {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
+          breakpoint: 768,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToScroll: 1
           }
         },
         {
