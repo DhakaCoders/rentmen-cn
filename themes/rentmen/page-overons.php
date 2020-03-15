3feac332abdd97aeeed0d6f44d70a3722da90b32
azizulsoftsw <azizul.softsw@gmail.com>
2020-03-15 23:20:31 +0600

    Create page-overons.php

---
+++ themes/rentmen/page-overons.php
@@ -0,0 +1,175 @@
+<?php 
+/*
+  Template Name: Overons
+*/
+get_header(); 
+$thisID = get_the_ID();
+?>
+<section class="breadcrumbs-sec">
+  <div class="container-lg">
+    <div class="row">
+      <div class="col-12">
+        <div class="breadcrumbs-innr hide-xs clearfix">
+          <div class="breadcrumbs-lft-text">
+            <strong class="page-title">Over Ons</strong>
+          </div>          
+          <div class="breadcrumbs-main">
+            <ul>           
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Binnenpagina</a></li>
+              <li><a href="#">Binnenpagina</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="breadcrumbs-innr show-xs clearfix">
+          <div class="breadcrumbs-left">
+            <a href="#">Home</a>
+          </div>
+          <div class="breadcrumbs-right">
+            <a href="javascript:history.go(-1)">Terug</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>    
+</section>
+
+<section class="overons-two-part-sec-wrp clearfix">
+  <div class="overons-two-part-lft clearfix">
+    <div class="overons-two-part-lft-img" style="background:url(<?php echo THEME_URI; ?>/assets/images/overons-two-part-lft-img.png);">
+    </div>
+  </div>
+  <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="overons-two-part-rgt">
+            <div class="ovr-two-grid-dsc">
+              <div class="ovr-two-grid-dsc-title">
+                <span>Mauris lacinia malesuada</span>
+                <strong>LOREM IPSUM DOLOR</strong>
+              </div>
+              <p>Suspendisse ligula augue, maximus eget sem ut, tempor malesuada odio. Aliquam facilisis massa vitae arcu malesuada molestie. Mauris lacinia malesuada elit, nec rhoncus dui vehicula id. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+              <p>Ut mi augue, auctor eu ante ac, feugiat blandit augue. Nullam luctus dolor velit, ac porttitor tortor convallis at. Duis blandit lorem nisi, vitae maximus.</p>
+              <blockquote>
+                Mauris in erat volutpat id morbi. Pulvinar et pretium odio turpis. Libero ipsum commodo senectus purus.
+              </blockquote>
+              <p>Vitae fringilla neque pulvinar non adipiscing feugiat. A tristique tristique vitae integer donec nisl enim ipsum aenean. Mauris in erat volutpat id morbi. Pulvinar et pretium odio turpis. Libero ipsum commodo senectus purus, nibh vitae. Pharetra, lobortis volutpat lectus lacus massa commodo cursus aliquam ut. </p>
+              <a href="#">Producten</a>
+              <a href="#">Contacteer ons</a>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>    
+</section>
+
+<section class="overons-service-sec-wrp">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="overons-justo-box">
+          <h2>Cras ut justo porta nisl rutrum porta.</h2>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="overons-service-wrp clearfix">
+          <ul class="reset-list dft-slider-pagi clearfix">
+            <li>
+              <div class="overons-service-box-dsc">
+                <i class="mHc"><img src="<?php echo THEME_URI; ?>/assets/images/overons-service-icon-1.svg"></i>
+                <h4>Auctor eu ante ac</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mi augue, auctor eu ante ac.</p>
+              </div>
+            </li>
+            <li>
+              <div class="overons-service-box-dsc">
+                <i class="mHc"><img src="<?php echo THEME_URI; ?>/assets/images/overons-service-icon-2.svg"></i>
+                <h4>Maecenas dolor</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mi augue, auctor eu ante ac.</p>
+              </div>
+            </li>
+            <li>
+              <div class="overons-service-box-dsc">
+                <i class="mHc"><img src="<?php echo THEME_URI; ?>/assets/images/overons-service-icon-3.svg"></i>
+                <h4>Lobortis et odio</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mi augue, auctor eu ante ac.</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="hm-de-kijker-post-sec-wrp overons-de-post-sec">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="hm-de-kijker-head">
+          <h2>Producten In De Kijker</h2>
+          <p>Morbi euismod blandit massa id congue. Mauris dignissim, augue ac maximus dapibus, enim ante facilisis odio, vel blandit tortor quam sit amet ante. Suspendisse a volutpat nulla.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="hm-de-kijker-post-wrp clearfix">
+          <ul class='reset-list clearfix'>
+            <li>
+              <div class="hm-de-kijker-post-innr">
+                <div class="hm-de-kijker-post-img-overflow">
+                  <a href="#" class="overlay-link"></a>
+                  <div class="hm-de-kijker-post-img" style="background: url(<?php echo THEME_URI; ?>/assets/images/hm-de-kijker-post-img-1.png);"></div>
+                </div>
+                <div class="hm-de-kijker-post-dsc">
+                  <a href="#">Meubilair</a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="hm-de-kijker-post-innr">
+                <div class="hm-de-kijker-post-img-overflow">
+                  <a href="#" class="overlay-link"></a>
+                  <div class="hm-de-kijker-post-img" style="background: url(<?php echo THEME_URI; ?>/assets/images/hm-de-kijker-post-img-2.png);"></div>
+                </div>
+                <div class="hm-de-kijker-post-dsc">
+                  <a href="#">Gedekte tafel</a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="hm-de-kijker-post-innr">
+                <div class="hm-de-kijker-post-img-overflow">
+                  <a href="#" class="overlay-link"></a>
+                  <div class="hm-de-kijker-post-img" style="background: url(<?php echo THEME_URI; ?>/assets/images/hm-de-kijker-post-img-3.png);"></div>
+                </div>
+                <div class="hm-de-kijker-post-dsc">
+                  <a href="#">Keukenmateriaal</a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="hm-de-kijker-post-innr">
+                <div class="hm-de-kijker-post-img-overflow">
+                  <a href="#" class="overlay-link"></a>
+                  <div class="hm-de-kijker-post-img" style="background: url(<?php echo THEME_URI; ?>/assets/images/hm-de-kijker-post-img-4.png);"></div>
+                </div>
+                <div class="hm-de-kijker-post-dsc">
+                  <a href="#">Partytenten</a>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="hm-download-btn">
+          <a href="#">BEKIJK ALLES</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php get_footer(); ?>
